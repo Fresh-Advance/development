fa7667f89560d2bd601aacbd295a85cffe997572
Anton Fedurtsya <anton@fedurtsya.com>
2023-10-09 21:35:43 +0300

    Add phpunit recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ recipes/default/phpunit/examples/tests/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ recipes/default/phpunit/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]});
+ROOT_PATH="$SCRIPT_PATH/../../..";
+
+make reset
+make setup
+make addbasicservices
+
+test -d $ROOT_PATH/source || (mkdir -p $ROOT_PATH/source && cp -r $SCRIPT_PATH/examples/* $ROOT_PATH/source/)
+
+make up
+
+docker-compose exec -T php composer update --no-interaction
+docker-compose exec -T php composer require phpunit/phpunit --no-interaction
+
+echo "Done!"
